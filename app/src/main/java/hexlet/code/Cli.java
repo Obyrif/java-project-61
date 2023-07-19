e9be857a3d558a299b8a409b8ab2dddf39d6f6cb
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-07-19 17:41:09 +0800

    add Scanner

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void getUserName(){
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.nextLine();
+        System.out.println("Hello, "+userName+"!");
+    }
+}
