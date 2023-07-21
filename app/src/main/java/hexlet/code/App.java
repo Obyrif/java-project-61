ff5b228a6729c493fdb3376c3c5cc4cc0c91075f
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-07-21 09:59:08 +0800

    add Even

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
-import hexlet.code.Cli;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.getUserName();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
+        if (sc.hasNext()) {
+            int player = sc.nextInt();
+            System.out.println("Your choice: " + player + "\n");
+            if (player == 1) {
+                Cli.getUserName();
+            } else if (player == 2) {
+                Even.getEven();
+            } else if (player == 0) {
+                sc.close();
+            }
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,30 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Even {
+    public static void getEven() {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc1 = new Scanner(System.in);
+        String userName = sc1.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        for (int i = 0; i < 3; i++) {
+            int number = random.nextInt(100) + 1;
+            System.out.println("Question: " + number);
+            String answer = sc.nextLine();
+            System.out.println("Your answer: " + answer);
+            String correctAnswer = (number % 2 == 0) ? "yes" : "no";
+            if (answer.equalsIgnoreCase(correctAnswer)) {
+                System.out.println("Correct, " + userName + "!");
+            } else {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'.\n" +
+                        "Let's try again, " + userName);
+            }
+        }
+    }
+}
