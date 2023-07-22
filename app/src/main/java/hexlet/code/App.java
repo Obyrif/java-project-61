399f5ab5eca9b41d7b23bafca2520b1fdaec9eb0
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-07-23 00:29:32 +0800

    add Calc

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
+        System.out.println("1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         if (sc.hasNext()) {
             int player = sc.nextInt();
             System.out.println("Your choice: " + player + "\n");
@@ -14,6 +14,8 @@ public class App {
                 Cli.getUserName();
             } else if (player == 2) {
                 Even.getEven();
+            } else if (player == 3) {
+                Calc.getCalc();
             } else if (player == 0) {
                 sc.close();
             }
---
+++ app/src/main/java/hexlet/code/Calc.java
@@ -0,0 +1,68 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Calc {
+    public static void getCalc() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String userName = sc.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        Random rd = new Random();
+
+        System.out.println("What is the result of the expression?");
+        int counter = 0;
+        while (counter < 3) {
+            int randomNumber = rd.nextInt(3);
+            int num1 = rd.nextInt(31);
+            int num2 = rd.nextInt(31);
+            switch (randomNumber) {
+                case 0:
+                    int sum = num1 + num2;
+                    System.out.println("Question: " + num1 + " + " + num2);
+                    int answerSum = sc.nextInt();
+                    System.out.println("Your answer: " + answerSum);
+                    if (sum == answerSum) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println(answerSum + " is wrong answer ;(. Correct answer was " + sum + ".\n"
+                                + "Let's try again.");
+                        System.exit(0);
+                    }
+                    break;
+                case 1:
+                    int minus = num1 - num2;
+                    System.out.println("Question: " + num1 + " - " + num2);
+                    int answerMinus = sc.nextInt();
+                    System.out.println("Your answer: " + answerMinus);
+                    if (minus == answerMinus) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println(answerMinus + " is wrong answer ;(. Correct answer was " + minus + ".\n"
+                                + "Let's try again.");
+                        System.exit(0);
+                    }
+                    break;
+                case 2:
+                    int even = num1 * num2;
+                    System.out.println("Question: " + num1 + " * " + num2);
+                    int answerEven = sc.nextInt();
+                    System.out.println("Your answer: " + answerEven);
+                    if (even == answerEven) {
+                        System.out.println("Correct!");
+                    } else {
+                        System.out.println(answerEven + " is wrong answer ;(. Correct answer was " + even + ".\n"
+                                + "Let's try again.");
+                        System.exit(0);
+                    }
+                    break;
+                default:
+                    System.out.println("Error");
+            }
+            counter++;
+        }
+        System.out.println("Congratulations! " + userName);
+    }
+}
