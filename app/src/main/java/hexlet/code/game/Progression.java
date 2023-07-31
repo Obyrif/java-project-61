e875a1c14aa3d6dbe71bda0faa2a158bfc977f52
Obydennov Dmitrii <dima-obydennov@mail.ru>
2023-07-31 18:46:47 +0800

    add

--- app/src/main/java/hexlet/code/game/Progression.java
+++ app/src/main/java/hexlet/code/game/Progression.java
@@ -8,6 +8,9 @@ public class Progression {
     private static final String DEST = "What number is missing in the progression?";
     private static final int MIN_LENGTH = 5;
     private static final int MAX_LENGTH = 10;
+    private static final int MAX_NUMBER = 100;
+    private static final int MAX_DIFFERENCE = 10;
+
 
     public static void getProgression() {
         String[][] qAndA = new String[Engine.ROUNDS][2];
@@ -19,8 +22,8 @@ public class Progression {
 
         for (int i = 0; i < Engine.ROUNDS; i++) {
             qAndA[i] = new String[2];
-            int firstNumber = random.nextInt(100);
-            int difference = random.nextInt(10) + 1;
+            int firstNumber = random.nextInt(MAX_NUMBER);
+            int difference = random.nextInt(MAX_DIFFERENCE) + 1;
 
             StringBuilder progression = new StringBuilder();
             for (int j = 0; j < progressionLength; j++) {
